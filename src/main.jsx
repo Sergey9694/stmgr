d8cecb0deec0de3ef11c2d23bd6fcc1cc1406a1f
Sergey Drobilenko <drobilenko94@mail.ru>
2023-05-24 16:39:53 +0300

    Создан store(передан в Provider, для прокидывания стора в компоненты), reducer, defaultState

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { createStore } from "redux";
+import { Provider } from "react-redux"; //модуль для связки компнентов реакта с редаксом
+
+//* action - js объект с полем type,
+//type - определяет как состояние будет изменяться
+// payload - поле для передачи любого количество данных
+// action = { type: "", payload: "?" };
+
+//* state - объект(чаще всего), либо  массив, либо примитив, который хранит данные
+//дефолтное состояние присваивается в тот момент когда пользователь открыл приложение и каждый раз когда в диспатч будем прокидывать экшн, состояние будет изменяться и храниться в store до тех пор, пока страница либо не обновится, либо не закроет приложение
+const defaultState = {
+    cash: 0,
+};
+
+//* reducer - js функция принмает состояние и экшн
+const reducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case "ADD_CASH":
+            return { ...state, cash: state.cash + action.payload };
+        case "GET_CASH":
+            return { ...state, cash: state.cash - action.payload };
+        // если пришел экшн с типом который не обрабатывается ни в 1 кейсе, то возвращаем неизмененное состояние(то которое есть на текущий момент)
+        default:
+            return state;
+    }
+};
+
+//* store - объект с методами (получиться состояние, изменить состояние, подписка на изменение состояния)
+const store = createStore(reducer);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
