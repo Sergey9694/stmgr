5982fe5e15d6ec168ecd8d3b165478e0111bf356
Sergey Drobilenko <drobilenko94@mail.ru>
2023-05-24 18:43:55 +0300

    devtools redux

--- src/store/index.js
+++ src/store/index.js
@@ -9,4 +9,9 @@ const rootReducer = combineReducers({
 });
 
 //* store - объект с методами (получиться состояние, изменить состояние, подписка на изменение состояния)
-export const store = createStore(rootReducer); // передали оба редьюсера в стор
+
+// для удобства разработки необходимо отслеживать изменение стейтса, для этого в стор можно передать 2-ым параметром как middleware, так и инструменты разработчика
+const devToolRedux = window._REDUX_DEVTOOLS_EXTENSION_
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : (f) => f;
+export const store = createStore(rootReducer, devToolRedux); // передали оба редьюсера в стор
