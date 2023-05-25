9738e0177e26d7f31d32931d2af6ea37e8fdd0f3
Sergey Drobilenko <drobilenko94@mail.ru>
2023-05-25 16:53:20 +0300

    создан функционал по добавлению и удалению пользователей (customerReducer)

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
 
     // Чтобы получить состояние
     const cash = useSelector((state) => state.cash.cash); //сюда передаем имя редьюсера.имя переменной
+    const customers = useSelector((state) => state.customer.customer); //сюда передаем имя редьюсера.имя переменной
 
     console.log(cash);
 
@@ -16,6 +17,18 @@ function App() {
     };
     const getCash = (cash) => {
         dispatch({ type: "GET_CASH", payload: cash });
+    };
+
+    const addCustomer = (name) => {
+        const customer = {
+            name,
+            id: Date.now(),
+        };
+        dispatch({ type: "ADD_CUSTOMER", payload: customer });
+    };
+
+    const removeCustomer = (customer) => {
+        dispatch({ type: "REMOVE_CUSTOMER", payload: customer.id });
     };
 
     return (
@@ -28,7 +41,33 @@ function App() {
                 <button onClick={() => getCash(Number(prompt()))}>
                     Снять со счета
                 </button>
+                <button onClick={() => addCustomer(prompt())}>
+                    Добавить клиента
+                </button>
+                <button onClick={() => getCash(Number(prompt()))}>
+                    Удалить клиента
+                </button>
             </div>
+            {customers.length > 0 ? (
+                <div>
+                    {customers.map((customer) => (
+                        <div
+                            key={customer.id}
+                            style={{
+                                fontSize: "3rem",
+                                border: "1px solid black",
+                                padding: "10px",
+                                margin: "10px",
+                            }}
+                            onClick={() => removeCustomer(customer)}
+                        >
+                            {customer.name}
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <div style={{ fontSize: "3rem" }}>Клиенты отсутствуют</div>
+            )}
         </div>
     );
 }
--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -6,9 +6,14 @@ const defaultState = {
 export const customerReducer = (state = defaultState, action) => {
     switch (action.type) {
         case "ADD_CUSTOMER":
-            return { ...state, cash: state.cash + action.payload };
-        case "GET_CUSTOMER":
-            return { ...state, cash: state.cash - action.payload };
+            return { ...state, customer: [...state.customer, action.payload] };
+        case "REMOVE_CUSTOMER":
+            return {
+                ...state,
+                customer: state.customer.filter(
+                    (customer) => customer.id !== action.payload
+                ),
+            };
         // если пришел экшн с типом который не обрабатывается ни в 1 кейсе, то возвращаем неизмененное состояние(то которое есть на текущий момент)
         default:
             return state;
